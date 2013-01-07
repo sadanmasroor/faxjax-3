bcae81c7b1ffcf827a8570d87b5341e0b57adc25
Sadan Masroor <sadanmasroor87@gmail.com>
2013-01-07 09:03:53 +0500

    Modified the files for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,5 +33,21 @@ namespace :deploy do
     puts "Now edit the config files in #{shared_path}."
   end
   after "deploy:setup", "deploy:setup_config"
-  # ...
+  
+  task :symlink_config, :roles => :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, :roles => :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
+  
+  
 end
